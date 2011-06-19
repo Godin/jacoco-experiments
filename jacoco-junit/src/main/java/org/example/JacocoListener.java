50c1764cdb34d563dcf1bab694e69969bf8aa545
Evgeny Mandrikov <mandrikov@gmail.com>
2011-06-19 04:44:39 +0400

    Use new features provided by JaCoCo 0.5.3
    * Use official jacoco-maven-plugin
    
    * Use MBeanController instead of TcpServerController, so remove strong
      dependency on JaCoCo API

--- jacoco-junit/src/main/java/org/example/JacocoListener.java
+++ jacoco-junit/src/main/java/org/example/JacocoListener.java
@@ -1,53 +1,46 @@
 package org.example;
 
-import org.jacoco.core.runtime.RemoteControlReader;
-import org.jacoco.core.runtime.RemoteControlWriter;
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunListener;
 
-import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class JacocoListener extends RunListener {
 
-  private static final String ADDRESS = "localhost";
-  private static final int PORT = 6300;
+  public JacocoListener() throws Exception {
+    System.out.println("Using JaCoCo " + JacocoController.getVersion());
+  }
 
   @Override
   public void testStarted(Description description) throws Exception {
-    System.out.println("Test " + getName(description) + " started");
+    String name = getName(description);
+    System.out.println("Test " + name + " started");
+
+    JacocoController.setSessionId(name);
+    JacocoController.reset();
   }
 
   @Override
   public void testFinished(Description description) throws Exception {
-    System.out.println("Test " + getName(description) + " finished");
-    save("target/jacoco/" + getName(description) + ".exec");
+    dump("target/jacoco.exec", true, true);
+
+    String name = getName(description);
+    System.out.println("Test " + name + " finished");
   }
 
-  private String getName(Description description) {
+  private static String getName(Description description) {
     return description.getClassName() + "." + description.getMethodName();
   }
 
-  private void save(String filename) throws IOException {
-    new File(filename).getParentFile().mkdirs();
-    final FileOutputStream localFile = new FileOutputStream(filename);
-    final RemoteControlWriter localWriter = new RemoteControlWriter(localFile);
-
-    // Open a socket to the coverage agent:
-    final Socket socket = new Socket(InetAddress.getByName(ADDRESS), PORT);
-    final RemoteControlWriter writer = new RemoteControlWriter(socket.getOutputStream());
-    final RemoteControlReader reader = new RemoteControlReader(socket.getInputStream());
-    reader.setSessionInfoVisitor(localWriter);
-    reader.setExecutionDataVisitor(localWriter);
-
-    // Send a dump and reset commands and read the response:
-    writer.visitDumpCommand(true, true);
-    reader.read();
-
-    socket.close();
-    localFile.close();
+  private static void dump(String destfile, boolean append, boolean reset) throws Exception {
+    byte[] dump = JacocoController.dump(reset);
+    // Save dump
+    new File(destfile).getParentFile().mkdirs();
+    OutputStream output = new BufferedOutputStream(new FileOutputStream(destfile, true));
+    output.write(dump);
+    output.close();
   }
 }
